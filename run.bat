cb1db720a339fef90b25ce90c8214732c6f64725
Стефан Йовев <stefanyovev@users.noreply.github.com>
2020-02-08 13:37:55 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
-@tcc -run brace.c %1>tmp.c
-@tcc "-run -llibportaudio64bit" tmp.c %2 %3 %4 %5 %6 %7 %8 %9
-@del tmp.c
+@tcc -run brace.c %1>%1.c
+@tcc "-run -llibportaudio64bit" %1.c %2 %3 %4 %5 %6 %7 %8 %9
 @echo.
 @pause
