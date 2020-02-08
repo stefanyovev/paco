148aff077d1775bbdfb874bdbe5150b8a00afccb
Стефан Йовев <stefanyovev@users.noreply.github.com>
2020-02-08 13:43:04 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
+
+@rem ASSOC .IC files with this bat so you can 'execute document' in notepad2 or just double click it to run
+
 @tcc -run brace.c %1>%1.c
 @tcc "-run -llibportaudio64bit" %1.c %2 %3 %4 %5 %6 %7 %8 %9
 @echo.
