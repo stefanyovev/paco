ea21ec6582c796e373a25ea90fd83620488432da
Стефан Йовев <stefanyovev@users.noreply.github.com>
2020-03-12 18:23:27 +0200

    ya man

--- run.bat
+++ run.bat
@@ -2,9 +2,9 @@
 @rem ASSOC .IC files with this bat so you can 'execute document' in notepad2 or just double click it to run
 
 tcc -run brace.c %1>%1.c
-tcc -llibportaudio64bit %1.c -o paco3.exe
+tcc -llibportaudio64bit %1.c -o paco2.exe
 
-paco3.exe
+paco2.exe
 
 @echo.
 @pause
