3b8ddae025ef704472231cf789d24362b8376a9f
Стефан Йовев <stefanyovev@users.noreply.github.com>
2020-10-29 16:37:57 +0200

    Add files via upload

--- run.bat
+++ run.bat
@@ -2,9 +2,9 @@
 @rem ASSOC .IC files with this bat so you can 'execute document' in notepad2 or just double click it to run
 
 tcc -run brace.c %1>%1.c
-tcc -llibportaudio64bit %1.c -o paco2.exe
+tcc -llibportaudio64bit %1.c -o pac.exe
 
-paco2.exe
+pac.exe
 
 @echo.
 @pause
