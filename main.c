686a555cbe14267848b3db1809e7bcede58c4262
Стефан Йовев <stefanyovev@users.noreply.github.com>
2021-11-14 15:44:46 +0200

    -

--- main.c
+++ main.c
@@ -13,12 +13,13 @@
 
 		#define SR 44100
 		
-		int srate = SR;				// sample rate [samples/second]
-		double stime = 1.0 / SR;	// sample duration [seconds]
-		int ssize = sizeof(float);	// sample size [bytes]
-		int asize = 1000;			// desired callback argument size (latency) [samples]
-		int hsize = SR*2;			// history size [samples]
-		int max_asize = 0;			// max play latency global [samples]
+		int srate = SR;					// sample rate [samples/second]
+		double stime = 1.0 / SR;		// sample duration [seconds]
+		int ssize = sizeof(float);		// sample size [bytes]
+		int asize = 1000;				// desired callback argument size (latency) [samples]
+		int hsize = SR*2;				// history size [samples]
+		int max_in_asize = 0;			// max rec latency [samples]
+		int max_out_asize = 0;			// max play latency [samples]
 
 		struct route {
 			int sd, sc;
@@ -33,13 +34,12 @@
 			route *outs;
 			PaStream *stream;
 			const PaStreamInfo *stream_info;
-			int aasize;			// actual asize
 			double t0, t;
 			long long in_len, out_len; };
 		typedef struct device device;
 
-		int ndevs = 0;
 		device *devs = 0;
+		int ndevs = 0;
 
 		int init() {
 			if( Pa_Initialize() ) {
@@ -62,7 +62,7 @@
 				devs[i].stream = 0;
 				devs[i].in_len = 0;
 				devs[i].out_len = 0;
-				devs[i].t0 = .0; }
+				devs[i].t0 = 0.0; }
 			PaUtil_InitializeClock(); }
 
 		PaStreamCallbackResult device_tick(
@@ -71,31 +71,31 @@
 			unsigned long frameCount,
 			const PaStreamCallbackTimeInfo *timeInfo,
 			PaStreamCallbackFlags statusFlags,
-			void *userData ) {
+			void *userData ){
 			
-			double t = PaUtil_GetTime();
-
 			device* dev = (device*) userData;
+			
 			float **in_data = input;
 			float **out_data = output;
-						
-			if( dev->t0 == 0.0 )
-				dev->t0 = t;
 
-			if( input ) {
+			if( input ){
 				int ofs = dev->in_len % hsize;
-				if( frameCount <= hsize -ofs ) {
-					for( int i=0; i< dev->nins; i++ ) {
-						memcpy( dev->ins +i*hsize +ofs, in_data[i], frameCount *ssize ); }}
+				if( frameCount <= hsize -ofs )
+					for( int i=0; i< dev->nins; i++ )
+						memcpy( dev->ins +i*hsize +ofs, in_data[i], frameCount *ssize ); 
 				else {
 					int x = ofs +frameCount -hsize;
-					for( int i=0; i< dev->nins; i++ ) {
+					for( int i=0; i< dev->nins; i++ ){
 						memcpy( dev->ins +i*hsize +ofs, in_data[i], (frameCount -x) *ssize );
 						memcpy( dev->ins +i*hsize, in_data[i]+x, x *ssize ); }}
-				dev->in_len += frameCount; }
+				dev->in_len += frameCount;
+				if( dev->t0 == 0.0 )
+					dev->t0 = PaUtil_GetTime();
+				if( max_in_asize < frameCount )
+					max_in_asize = frameCount; }
 				
-			if( output ) {				
-				for( int dc=0; dc<dev->nouts; dc++ ) {
+			if( output )
+				for( int dc=0; dc<dev->nouts; dc++ ){
 					int dd = dev->id;
 					int sd = dev->outs[dc].sd;
 					int sc = dev->outs[dc].sc;
@@ -104,35 +104,38 @@
 						// TODO: zeromem
 						continue;
 					
-					if( !devs[sd].in_len ) {
-						printf( "%d wating %d to start \n", dd, sd );
+					if( !devs[sd].in_len ){
+						printf( "%d waiting %d to start \n", dd, sd );
 						continue; }
 
 					long long src;
-					
-					if( dev->outs[dc].last_src == 0 ) {
-						src = devs[sd].in_len -frameCount*2; }
-					else {
-						src = dev->outs[dc].last_src; }
+					if( dev->outs[dc].last_src == 0 )
+						src = devs[sd].in_len -frameCount*2;
+					else
+						src = dev->outs[dc].last_src;
 						
-					if( src < 0  ) {
-						printf( "%d buffering %d src %d t-t0 %d t %10.10f \n", dd, sd, src, (int)(ceil( (t -devs[sd].t0) /stime )), t );
+					if( src < 0  ){
+						printf( "%d buffering %d src %d t-t0 %d t %10.10f \n", dd, sd, src, (int)(ceil( (PaUtil_GetTime() -devs[sd].t0) /stime )), PaUtil_GetTime() );
 						continue; }
-
-					if( src +frameCount > devs[sd].in_len ) {
+					if( src +frameCount > devs[sd].in_len ){
 						printf( "%d wants to read %d future unsaved samples from %d \n", dd, src +frameCount -devs[sd].in_len, sd );
 						continue; }
 					
 					int ofs = src % hsize;
-					if( ofs +frameCount <= hsize ) {
-						memcpy( out_data[dc], devs[sd].ins +sc*hsize +ofs, frameCount *ssize ); }
+					if( ofs +frameCount <= hsize )
+						memcpy( out_data[dc], devs[sd].ins +sc*hsize +ofs, frameCount *ssize );
 					else {
 						int x = ofs +frameCount -hsize;
 						memcpy( out_data[dc], devs[sd].ins +sc*hsize +ofs, x*ssize );
 						memcpy( out_data[dc] +x, devs[sd].ins +sc*hsize, (frameCount -x)*ssize ); }
+						
 					dev->outs[dc].last_src = src +frameCount; }
 						
-				dev->out_len += frameCount; }
+				dev->out_len += frameCount;
+				if( dev->t0 == 0.0 )
+					dev->t0 = PaUtil_GetTime();
+				if( max_out_asize < frameCount )
+					max_out_asize = frameCount;
 
 			return paContinue; }
 				
@@ -157,7 +160,6 @@
 				&(dev->stream), dev->nins ? &in_params : 0, dev->nouts ? &out_params : 0,
 				srate, paFramesPerBufferUnspecified, paClipOff|paDitherOff,
 				&device_tick, dev );
-				
 			if( err != paNoError ) {
 				if( err != paUnanticipatedHostError ) {
 					printf( "ERROR 1: %s \n", Pa_GetErrorText( err ) ); }
@@ -165,16 +167,7 @@
 					const PaHostErrorInfo* herr = Pa_GetLastHostErrorInfo();
 					printf( "ERROR 2: %s \n", herr->errorText ); }}
 			
-			// TODO:
-			dev->stream_info = Pa_GetStreamInfo( dev->stream );
-			int actual_srate = (int) round( dev->stream_info->sampleRate );
-			int actual_asize = (int) round( (dev->nins?(dev->stream_info->inputLatency):(dev->stream_info->outputLatency))/stime );
-			dev->aasize = actual_asize;
-			if( actual_asize > max_asize ) max_asize = actual_asize;
-			printf( "%d starting %s%s SR %d asize %d \n", dev->id, dev->nins?"rec":"", dev->nouts?"play":"", actual_srate, actual_asize );
-			
 			err = Pa_StartStream( dev->stream );
-			
 			if( err != paNoError ) {
 				printf( "ERROR 3: %s \n", Pa_GetErrorText( err ) ); }}
 
