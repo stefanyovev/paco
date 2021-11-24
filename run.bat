1915273087b01f0cc79b00e22a2789c80b20d540
Стефан Йовев <stefanyovev@users.noreply.github.com>
2021-11-24 13:25:12 +0200

    -

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
-@tcc -llibportaudio64bit main.c -o main.exe
-@if %errorlevel% == 0 ( @main.exe )
-
-
 @echo.
-@pause
+
+@tcc -llibportaudio64bit main.c -o main.exe
+
+@if %errorlevel% == 0 ( @main.exe ) else ( @echo. & @pause )
