039198761b84734c111babe3e7ad361b33180375
Стефан Йовев <stefanyovev@users.noreply.github.com>
2023-01-02 02:55:33 +0200

    -

--- ui.py
+++ ui.py
@@ -40,14 +40,16 @@ def box(typ, content):
         layout.addWidget(w)
     return widget
 
+
 def Left(*content):
     return box('left', content)
+
 
 def Top(*content):
     return box('top', content)
 
 
-class ReadLoop(Thread):
+class ReadThread(Thread):
 
     sig_receive = signal(str)
 
@@ -63,7 +65,7 @@ class ReadLoop(Thread):
             if not new_data:
                 break
             self.sig_receive.emit(new_data)
-        print('\n[ReadLoop DONE]\n')
+        print('\n[%s DONE]\n' % self.handle)
 
 
 class Process(Thread):
@@ -76,8 +78,7 @@ class Process(Thread):
         self.sig_stop.connect(stop_func)
         self.sig_receive.connect(receive_func)
         self.proc = Popen(cmd, shell=False, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        self.readloop = ReadLoop(self.proc.stdout, self.receive)
-        self.sendbuf = self.recvbuf = ''
+        self.readthread = ReadThread(self.proc.stdout, self.sig_receive)
         self.start()
 
     def run(self):
@@ -88,70 +89,24 @@ class Process(Thread):
                 self.sig_stop.emit(retcode)
                 break
             sleep(0.0333)
+        print('\n[PROC DONE]\n')
 
     def send(self, data):
-        self.sendbuf += data + '\n'
-        if '\n' in self.sendbuf:
-            readydata, self.sendbuf = self.sendbuf.rsplit('\n', 1)
-            readydata += '\n'
-            self.proc.stdin.write(bytes(readydata, encoding='ascii'))
-            try: self.proc.stdin.flush()
-            except: pass
-
-    def receive(self, data):
-        self.recvbuf += data
-        if '\n' in self.recvbuf:
-            parts = self.recvbuf.split('\n')
-            self.recvbuf = parts[-1]
-            for line in parts[:-1]:
-                self.sig_receive.emit(line.strip('\r'))
+        self.proc.stdin.write(bytes(data, encoding='ascii'))
+        try: self.proc.stdin.flush()
+        except: print('\n[err flushing %s]' % self.handle)
 
     def stop(self):
         self.proc.kill()
 
 
-class GUIShellApp(Object):
-    """ To be Extended.
-        __init__ assumes class member 'cmd'.
-        __init__ assumes instance member 'ui'. """
-        
-    sig_core_output = signal(str)
-    sig_core_stopped = signal(int)
-
-    def __init__(self):
-        super().__init__()
-    
-        self.core = Process(
-            self.cmd,
-            self.sig_core_output,
-            self.sig_core_stopped)
-        
-        self.ui.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
-        self.ui.setWindowTitle(self.cmd+' UI')
-        self.ui.setObjectName('root')
-        self.ui.closeEvent = self.ui_close_event        
-        self.ui.show()
-
-    def stop(self):
-        self.core.stop()
-
-    def ui_close_event(self, e):
-        self.stop()
-
-    def exec(self):
-        return self.app.exec()
-
-
 # ----------------------------------------------------------------------------
 
 
-class PACO_GUI(GUIShellApp):
+class PACO_GUI(Application):
 
-    cmd = "main.exe"
-    
     def __init__(self):
-
-        self.app = Application([])
+        super().__init__([])
         
         self.label1 = Label('From:', objectName='label1')
         self.label2 = Label('To:', objectName='label2')
@@ -160,7 +115,7 @@ class PACO_GUI(GUIShellApp):
         self.button = Button('Play', objectName='button')
 
         self.ui = \
-          Top(
+          Top('root',
               Left('head',
                   Top(
                       Left( self.label1, self.combo1 ),
@@ -184,10 +139,17 @@ class PACO_GUI(GUIShellApp):
             min-height: 100px; max-height: 100px; }
           """)
 
-        super().__init__()
+        self.ui.setWindowTitle('PortAudio Console')
+        self.ui.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
+        self.ui.show()
+
+        self.core = Process('main.exe', self.on_receive, exit)
+        self.stdin_observer = ReadThread(sys.stdin.buffer, self.core.send)
+
         self.button.clicked.connect(self.on_play_clicked)
-        self.sig_core_output.connect(self.on_receive)
-        self.sig_core_stopped.connect(exit)
+        self.ui.closeEvent = self.closeEvent
+
+        self.recvbuf = ''
         self.routes = []
         self.rows = []
           
@@ -198,40 +160,47 @@ class PACO_GUI(GUIShellApp):
         slider.setRange(0, 10000)
         slider.valueChanged.connect(partial(self.set_route_delay, sd, dd))
         row = Top(label1, Left(label2, slider))
-        row.setStyleSheet(""" max-height: 100px; """)
+        row.setStyleSheet(""" max-height: 100px; background-color: gray; """)
         self.rows.append(row)
         self.ui.layout().addWidget(row)
     
     def set_route_delay(self, sd, dd, val):
-        cmd = '%s 0 %s 0 %s\n%s 1 %s 1 %s' % (sd, dd, val, sd, dd, val)
+        cmd = '%s 0 %s 0 %s\n%s 1 %s 1 %s\n' % (sd, dd, val, sd, dd, val)
+        print(cmd, end='')
         self.core.send(cmd)
 
     def on_play_clicked(self, e):
         sd = fullmatch("\s+([0-9]+)\s+.*", self.combo1.currentText()).groups()[0]
         dd = fullmatch("\s+([0-9]+)\s+.*", self.combo2.currentText()).groups()[0]
         self.routes.append((sd, dd))
-        cmd = '%s 0 %s 0\n%s 1 %s 1' % (sd, dd, sd, dd)
+        cmd = '%s 0 %s 0\n%s 1 %s 1\n' % (sd, dd, sd, dd)
+        print(cmd, end='')
         self.core.send(cmd)
         self.add_route_row(sd, dd)
 
     def on_receive(self, data):
-        print(data)
-        match = fullmatch("\s+([0-9]+)\s+([0-9]+|-)\s([0-9]+|-)\s+\"(.*)\"\s\"(.*)\".*", data)
-        if match:
-            g = match.groups()
-            name = "   %s[%s]   %s" % (g[0].ljust(5), g[3], g[4])
-            if g[1] != '-': self.combo1.addItem(name)
-            if g[2] != '-': self.combo2.addItem(name)
+        print(data, end='')
+        self.recvbuf += data
+        if '\n' not in self.recvbuf:
+            return
+        parts = self.recvbuf.split('\n')
+        self.recvbuf = parts[-1]
+        for line in parts[:-1]:
+            match = fullmatch("\s+([0-9]+)\s+([0-9]+|-)\s([0-9]+|-)\s+\"(.*)\"\s\"(.*)\".*", line.strip('\r'))
+            if match:
+                g = match.groups()
+                name = "   %s[%s]   %s" % (g[0].ljust(5), g[3], g[4])
+                if g[1] != '-': self.combo1.addItem(name)
+                if g[2] != '-': self.combo2.addItem(name)
+
+    def closeEvent(self, e):
+        self.core.stop()
 
 
 if __name__ == '__main__':
 
-    app = PACO_GUI()
-
-    stdin = ReadLoop(sys.stdin.buffer, app.core.send)
-
     try:
-        app.exec()
+        PACO_GUI().exec()
     except:
         print('ERR\n')
         sleep(5)
